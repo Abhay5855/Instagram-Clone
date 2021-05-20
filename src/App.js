9df2a623543785c0bf7b1557e35d8c03637624cf
Abhay5855 <ap3919529@gmail.com>
2021-05-20 19:19:58 +0530

    style: Styling the posts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-
-import './App.css';
-import Navbar from './components/Navbar';
-import Posts from './components/Posts';
+import { useState, useEffect } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Posts from "./components/Posts";
+import { db } from "./firebase";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = () => {
+    db.collection("posts").onSnapshot((snapshot) => {
+      setPosts(snapshot.docs.map((doc) => doc.data()));
+    });
+  };
+
+  // useeffect for posts
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  console.log(posts);
+
   return (
     <div className="App">
-
-
-    <div className="nav__bar">
+      <div className="nav__bar">
         <Navbar />
-    </div>
+      </div>
 
-    <div className="header__post">
-         
-    <Posts />
-    </div>
-
-   
-
-     
+      {posts.map((post) => (
+        <Posts
+          key={post.id}
+          username={post.username}
+          id={post.id}
+          imageUrl={post.imageUrl}
+          caption={post.caption}
+        />
+      ))}
     </div>
   );
 }
