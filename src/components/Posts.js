4febe67cecc16c7b987fe8d16e2ef34723645c4b
Abhay5855 <ap3919529@gmail.com>
2021-05-20 13:56:57 +0530

    create: Creating a new component for post

---
+++ src/components/Posts.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Posts() {
+    return (
+        <div className="Posts__container">
+
+        {/* userAvatar */}
+        {/* username */}
+        {/* Post-Image  */}
+        {/* username : caption */}
+            
+        </div>
+    )
+}
+
+export default Posts
