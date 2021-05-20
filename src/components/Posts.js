1d0cab90c5d5be748f0949a24f4804feb70829ed
Abhay5855 <ap3919529@gmail.com>
2021-05-20 19:19:41 +0530

    docs: Passing the uname,img,comment as props

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Avatar from "@material-ui/core/Avatar";
 import  "./Posts.css"
-function Posts() {
+function Posts({username, id, caption, imageUrl}) {
   return (
     <div className="Posts__container">
-      <div className="Posts__details">
-        {/* userAvatar */}
+      
+       <div className="Posts__details">
+          {/* userAvatar */}
 
         <Avatar
           alt="Remy Sharp"
@@ -15,23 +16,27 @@ function Posts() {
 
         {/* username */}
 
-        <h3>Username</h3>
-      </div>
+        <h3>{username}</h3>
+     
+       </div>
+        
 
-      <div className="Posts__images">
-        {/* Post-Image  */}
-        <img
-          className="images"
-          src="https://scontent.fnag4-1.fna.fbcdn.net/v/t1.6435-9/44890935_2201405306807547_4333259957395783680_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=84a396&_nc_ohc=LrnsAL9YvGYAX8lNoix&_nc_ht=scontent.fnag4-1.fna&oh=4f267fc32675f1d3a8ea0f148ff478fe&oe=60CC71AF"
-          alt="post"
+        <div className="Posts__images">
+             {/* Post-Image  */}
+        <img className="images"
+          src={imageUrl} alt="posts"
         ></img>
 
          {/* username : caption */}
 
          <h4 className="user__caption">
-             <strong>Username : </strong>Friends for life!
+             <strong>{username} : </strong>{caption}
          </h4>
-      </div>
+      
+        </div>
+
+      
+       
 
      
     </div>
