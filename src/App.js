83dda0c075356e43b64bb654805c080acb6f9228
Abhay5855 <ap3919529@gmail.com>
2021-05-20 14:25:05 +0530

    import: Importing the Posts component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
+import Posts from './components/Posts';
 
 function App() {
   return (
@@ -9,6 +10,11 @@ function App() {
 
     <div className="nav__bar">
         <Navbar />
+    </div>
+
+    <div className="header__post">
+         
+    <Posts />
     </div>
 
    
