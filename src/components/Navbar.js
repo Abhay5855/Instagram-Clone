95b4acba806b30a948e675ec0547d0efbf73121b
Abhay5855 <ap3919529@gmail.com>
2021-05-20 13:57:20 +0530

    create: Creating a Navbar component with logo

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Navbar.css"
+function Navbar() {
+    return (
+        <div className="Navbar__container">
+
+         <img className="Navbar__img" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="logo"></img>
+
+         
+
+            
+        </div>
+    )
+}
+
+export default Navbar
