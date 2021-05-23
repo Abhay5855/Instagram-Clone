223d316f7da2084372e5e4a492c0ab0a16889147
Abhay5855 <ap3919529@gmail.com>
2021-05-23 18:17:54 +0530

    initial commit

--- src/components/ChatScreen.js
+++ src/components/ChatScreen.js
@@ -11,15 +11,20 @@ function ChatScreen() {
       name: "Rutkar",
       image:
         "https://scontent.fnag4-1.fna.fbcdn.net/v/t1.6435-9/c0.0.866.866a/s851x315/89910138_562540641024679_3470197772597067776_n.jpg?_nc_cat=106&ccb=1-3&_nc_sid=da31f3&_nc_ohc=Ga0_buQszWgAX-AjD9Z&_nc_ht=scontent.fnag4-1.fna&tp=28&oh=a8424752d7a1e7b76cf478d78bf2a423&oe=60D0BCB5",
-      message: "Hey chamat",
+      message: "Hey",
     },
 
     {
       name: "Aakriti",
       image:
         "https://scontent.fnag4-1.fna.fbcdn.net/v/t1.6435-9/s851x315/67646391_1336935886469181_5890040801668366336_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=da31f3&_nc_ohc=dfN3LgcFAJEAX-siE0R&_nc_ht=scontent.fnag4-1.fna&tp=7&oh=b0fd3b4a53c07305781b1a95e741c845&oe=60CEE615",
-      message: "hey there",
+      message: "How are you",
     },
+    {
+      name:"Scarlett",
+      image:"https://m.media-amazon.com/images/M/MV5BMTM3OTUwMDYwNl5BMl5BanBnXkFtZTcwNTUyNzc3Nw@@._V1_.jpg",
+      message:"I love you"
+    }
   ]);
 
   const handleClick = (e) => {
